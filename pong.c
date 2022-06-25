5fae5dd9e9ebbc95ef388539bcc96e71e798e460
Sergey Bevza <37591122+sbevza@users.noreply.github.com>
2022-06-25 18:42:11 +0300

    Update pong.c

--- pong.c
+++ pong.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
-// include conio.h file for kbhit function
-#include <conio.h>
+#include <ncurses.h>
 #include <unistd.h>  //Для sleep
 
-void display(int cross_pos, int toe_pos);
-int pressing_key(void);
-void sleeping_time();
+#define PLAYER_SIZE 3
+
+#define TERM_SIZE_X 80
+#define TERM_SIZE_Y 25
+
+#define WINNING_SCORE 20
 
 struct Ball {
   int x;
@@ -17,140 +19,132 @@ struct Ball {
 struct Player {
   int x;
   int y;
-  int winer = 0;
-  int score = 0;
-  int size = 3;
-}
+  int winer;
+  int score;
+  int size;
+};
+
+void display(int, int);
+int pressing_key();
+void sleeping_time();
+void ball_movement(Ball);
+void players_movement(int, int, int);
+int has_winner(int, int);
 
 int main() {
-    int term_size_x = 80;
-    int term_size_y = 25;
-   
-    struct Player player1;
-    player1.x = 0;
-    player1.y = 0;
+  int term_size_x = 80;
+  int term_size_y = 25;
 
-    struct Player player2;
-    player2.x = 0;
-    player2.y = 0;
-    
-    int key_id = 0;
-    int esc_key = 27;
+  int player1_y = 12;
+  int player1_score = 0;
 
-    ball.x = 40;
-    ball.y = 12;
-    ball.direction.x = 1; // 0 - вправо, 1 - влево
-    ball.direction_y = 1; // 0 - вверх, 1 - вниз
-    
-    while (key_id != esc_key) {
-      if (has_winner(player1, player2)) {
-          break;
-      }
+  int player2_y = 12;
+  int player2_score = 0;
 
-      key_id = pressing_key();
-      players_movement(player1, player2, key_id);
-      
-      ball_movement(ball);
+  int key_id = 0;
+  int esc_key = 27;
 
-      display();
-      sleeping_time();
+  ball.x = 40;
+  ball.y = 12;
+  ball.direction_x = 1; // 0 - вправо, 1 - влево
+  ball.direction_y = 1; // 0 - вверх, 1 - вниз
+
+  while (key_id != esc_key) {
+    if (has_winner(player1_score, player2_score)) {
+      break;
     }
 
-    return 0;
+    key_id = pressing_key();
+    players_movement(player1_y, player2_y, key_id);
+    ball_movement(ball);
+
+    //display();
+    //sleeping_time();
+  }
+
+  return 0;
+}
+
+int has_winner(int player1, int player2) {
+  int result = 0;
+  if (player1 >= WINNING_SCORE || player2 >= WINNING_SCORE) {
+    result = player1 > player2 ? player1 : player2;
+  }
+  return result;
 }
 
 void ball_movement(Ball ball) {
-    ball.x += ball.direction_x == 1 ? 1 : -1;
-
-    ball.y += ball.direction_y == 1 ? 1 : -1;
-    if (ball_y >= term_size_y) {
-      ball_direction_y = 0;
-    }
-    if (ball_y <= 0) {
-      ball_direction_y = 1;
-    } 
+//  ball.x += ball.direction_x == 1 ? 1 : -1;
+//
+//  ball.y += ball.direction_y == 1 ? 1 : -1;
+//
+//  if (ball.y >= term_size_y) {
+//    ball_direction_y = 0;
+//  }
+//  if (ball.y <= 0) {
+//    ball_direction_y = 1;
+//  }
 }
 
-void players_movement(Player player1, Player player2, int key_id) {
-    switch (key_id) {
-case 'k':
-  if (player2.y > 0) {
-    player2.y--;
+void players_movement(int player1, int player2, int key_id) {
+
+  switch (key_id) {
+    case 'k':
+      if (player2 > 0) {
+        player2--;
+      }
+      break;
+    case 'm':
+      if ((player2 + PLAYER_SIZE) < TERM_SIZE_Y) {
+        player2++;
+      }
+      break;
+    case 'a':
+      if (player1 > 0) {
+        player1--;
+      }
+      break;
+    case 'z':
+      if ((player1 + PLAYER_SIZE) < TERM_SIZE_Y) {
+        player1++;
+      };
+      break;
+    default:break;
   }
-  break;
-case 'm':
-  if ((player2.y + player2.size) < term_size_y) {
-    player2.y++;
+
+}
+
+void display(Player player1, Player player2) {
+
+  char cross = 'X';
+  char toe = '0';
+  char space = ' ';
+
+  // int field[term_size_x][term_size_y] = {0};
+
+  for (int i = 1; i <= TERM_SIZE_Y; i++) {
+    for (int j = 1; j <= TERM_SIZE_X; j++) {
+//      if (i == "cross_pos") {
+//        printf("%c", cross);
+//      } else if (i == toe_pos) {
+//        printf("%c", toe);
+//      } else {
+//        printf("%c", space);
+    }
   }
-  break;
-case 'a':
-  if (player1.y > 0) {
-    player1.y--;
-  }
-  break;
-case 'z':
-  if ((player1.y + player1.size) < term_size_y) {
-    player1.y++;
-  };
-  break;
-default:
-  break;
-}
-   
 }
 
-void display(player1, player2) {
-    int term_size_x = 80;
-    int term_size_y = 25;
-
-    char cross = 'X';
-    char toe = '0';
-    char space = ' ';
-
-    int field [term_size_x][term_size_y] = {0};
-
-    for (int i = 1; i <= term_size_y; i++) {
-        for (int j = 1; j <= term_size_x; j++) {
-          if (i == cross_pos) {
-              printf("%c", cross);
-          } else if (i == toe_pos) {
-              printf("%c", toe);
-          } else {
-              printf("%c", space);
-          }
-        }
-    }
+void sleeping_time() {
+  sleep(1);
 }
 
-int pressing_key(void) {
-   // declare variable
+int pressing_key() {
 
-    char ch;
+  char c = 'a';
+  printf("Enter key or enter \"Q\" to exit:\n");
+  scanf("%c", &c);
 
-    printf("Enter key ESC to exit \n");
-
-    // define infinite loop for taking keys
-
-    while (1) {
-
-        if (kbhit) {
-
-            // fetch typed character into ch
-
-            ch = getch();
-
-            if ((int)ch == 27)
-
-                // when esc button is pressed, then it will exit from loop
-
-                break;
-
-            printf("You have entered : %c\n", ch);
-
-        }
-    }
-
-void sleeping_time() { 
-     sleep (1);
-    }
+  return c;
 }
+
+
